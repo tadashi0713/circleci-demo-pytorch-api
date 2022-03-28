27c934867bfbef44b2b24086ac0e264f177e4ab9
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-03-28 14:18:33 +0900

    Add zebra test

---
+++ tests/test_zebra.py
@@ -0,0 +1,10 @@
+from io import BytesIO
+import pytest
+from app import app
+
+def test_zebra():
+  with open('./images/zebra.jpeg', 'rb') as img:
+    img_string = BytesIO(img.read())
+  response = app.test_client().post('/predict', data={'file': (img_string, 'zebra.jpeg')},
+                      content_type="multipart/form-data")
+  assert response.json['class_name'] == 'horse'
