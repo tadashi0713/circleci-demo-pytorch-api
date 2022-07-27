b38129a2650d6a877458e4e501218461a3ae2ae0
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-07-28 01:31:51 +0900

    Fix test

--- tests/test_zebra.py
+++ tests/test_zebra.py
@@ -3,8 +3,8 @@ import pytest
 from app import app
 
 def test_zeebra():
-  with open('./images/zeebra.jpeg', 'rb') as img:
+  with open('./images/zebra.jpeg', 'rb') as img:
     img_string = BytesIO(img.read())
   response = app.test_client().post('/predict', data={'file': (img_string, 'zebra.jpeg')},
                       content_type="multipart/form-data")
-  assert response.json['class_name'] == 'horse'
+  assert response.json['class_name'] == 'zebra'
